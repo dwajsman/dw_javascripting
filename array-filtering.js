b1b23c30199a3b5c1b66560bf3c9f2cebbe52352
dwajsman <danielwajsman@mac.com>
2021-10-05 22:09:43 -0700

    first upload with javascripting challenge files

---
+++ array-filtering.js
@@ -0,0 +1,18 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let filtered = numbers.filter(function a (number){
+  return number % 2 === 0;
+});
+
+console.log(filtered);
+
+
+
+
+    //  const pets = ['cat', 'dog', 'elephant']  
+     
+    //  const filtered = pets.filter(function (pet) {  
+    //    return (pet !== 'elephant')  
+    //  })  
+
+    // console.log(filtered);
